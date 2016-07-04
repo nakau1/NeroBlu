a31612d6b849d66995fa569f46345f3514e2af2f
中安佑一 <yuichi.nakayasu@gmail.com>
2016-07-05 04:09:48 +0900

    アクセサクラスの実装

--- NeroBlu/NBRealmAccessor.swift
+++ NeroBlu/NBRealmAccessor.swift
@@ -46,12 +46,54 @@ public extension NBRealmAccessor {
 	}
 }
 
+// MARK: - レコード取得 -
+public extension NBRealmAccessor {
+    
+    /// ソート情報
+    /// - complexity: String フィールド名
+    /// - complexity: Bool 昇順: true、降順: false
+    public typealias NBRealmSort = [String : Bool]
+    
+    /// エンティティを配列で取得する
+    /// - parameter condition: 条件オブジェクト
+    /// - parameter sort: ソート
+    /// - returns: エンティティの配列
+    public func select(condition condition: NSPredicate? = nil, sort: NBRealmSort? = nil) -> [Entity] {
+        return self.getResult(condition: condition, sort: sort).map { return $0 }
+    }
+    
+    /// 指定した条件で抽出されるレコード数を取得する
+    /// - parameter condition: 条件オブジェクト
+    /// - parameter sort: ソート
+    /// - returns: レコード数
+    public func count(condition condition: NSPredicate? = nil) -> Int {
+        return self.getResult(condition: condition, sort: nil).count
+    }
+    
+    /// 指定した条件・ソートから結果オブジェクトを取得する
+    /// - parameter condition: 条件オブジェクト
+    /// - parameter sort: ソート
+    /// - returns: RealmResultsオブジェクト
+    private func getResult(condition condition: NSPredicate? = nil, sort: NBRealmSort? = nil) -> RealmSwift.Results<T> {
+        var result = self.realm.objects(Entity)
+        if let filter = condition {
+            result = result.filter(filter)
+        }
+        if let sort = sort {
+            sort.forEach {
+                result = result.sorted($0.0, ascending: $0.1)
+            }
+        }
+        return result
+    }
+}
+
 // MARK: - レコード追加 -
 public extension NBRealmAccessor {
 	
-	/// エンティティを一括で新規追加する
+	/// エンティティの配列からレコードを一括で新規追加する
 	/// - parameter entities: 追加するエンティティの配列
-	public func add(entities: [Entity]) {
+	public func insert(entities: [Entity]) {
 		let r = self.realm
 		var i = 0, id: Int64 = 1
 		try! r.write {
@@ -66,268 +108,76 @@ public extension NBRealmAccessor {
 		}
 	}
 	
-	/// エンティティを新規追加する
+	/// エンティティからレコードを新規追加する
 	/// - parameter entity: 追加するエンティティ
-	public func add(entity: Entity) {
-		self.add([entity])
+	public func insert(entity: Entity) {
+		self.insert([entity])
 	}
 }
 
+// MARK: - レコード削除 -
+public extension NBRealmAccessor {
+    
+    /// 指定した条件に該当するレコードを削除する
+    /// - parameter condition: 条件オブジェクト
+    public func delete(condition: NSPredicate) {
+        let r = self.realm
+        try! r.write {
+            r.delete(r.objects(Entity).filter(condition))
+        }
+    }
+    
+    /// 指定したIDに該当するレコードを削除する
+    /// - parameter ids: IDの配列
+    public func delete(ids ids: [Int64]) {
+        
+    }
+    
+    /// 指定したエンティティのレコードを削除する
+    /// - parameter entity: エンティティ
+    public func delete(entity entity: Entity) {
+        
+    }
+}
 
-/*
 // MARK: データ更新
 public extension NBRealmAccessor {
     
-    /// 指定した条件のRealmオブジェクトを一括で更新する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter predicate: 条件
-    /// - parameter modify: 更新処理
-    public func modifyWithPredicate<T : NBRealmObject>(type: T.Type, predicate: NSPredicate, modify: (T, Int) -> Void) {
+    /// データ更新クロージャ
+    /// - complexity: Entity 更新対象のエンティティ
+    /// - complexity: Int インデックス
+    public typealias NBRealmUpdateClosure = (Entity, Int) -> Void
+    
+    /// 指定した条件で抽出されるレコードを更新する
+    /// - parameter condition: 条件オブジェクト
+    /// - parameter updating: データ更新クロージャ
+    public func update(condition: NSPredicate? = nil, updating: NBRealmUpdateClosure) {
         let r = self.realm
         try! r.write {
-            let result = r.objects(type).filter(predicate)
+            var result = r.objects(Entity)
+            if let condition = condition {
+                result = result.filter(condition)
+            }
             var i = 0
-            for realmObject in result {
-                realmObject.modified = NSDate()
-                modify(realmObject, i)
+            for entity in result {
+                entity.modified = NSDate()
+                updating(entity, i)
                 i += 1
             }
         }
     }
     
-    /// 指定したIDのRealmオブジェクトを一括で更新する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter ids: IDの配列
-    /// - parameter modify: 更新処理
-    public func modifyByIDs<T : NBRealmObject>(type: T.Type, ids: [Int64], modify: (T, Int) -> Void) {
-        self.modifyWithPredicate(type, predicate: self.predicateIDs(ids), modify: modify)
-    }
-    
-    /// Realmオブジェクトの配列を渡して一括で更新する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter realmObjects: 更新するRealmオブジェクトの配列
-    /// - parameter modify: 更新処理
-    public func modify<T : NBRealmObject>(type: T.Type, _ realmObjects: [T], modify: (T, Int) -> Void) {
-        let ids = realmObjects.map { return $0.id }
-        self.modifyByIDs(type, ids: ids, modify: modify)
-    }
-    
-    /// RealmオブジェクトのRealmSwift.List、またはRealmSwift.Resultを渡して一括で更新する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter realmObjects: 更新するRealmオブジェクトの配列
-    /// - parameter modify: 更新処理
-    public func modify<T : NBRealmObject, S: SequenceType where S.Generator.Element: NBRealmObject>(type: T.Type, _ realmObjects: S, modify: (T, Int) -> Void) {
-        let array = realmObjects.map { return $0 as NBRealmObject }
-        self.modify(type, array, modify: modify)
-    }
-    
-    /// 渡したRealmオブジェクトを更新する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter realmObject: 更新するRealmオブジェクト
-    /// - parameter modify: 更新処理
-    public func modify<T : NBRealmObject>(type: T.Type, _ realmObject: T, modify: (T, Int) -> Void) {
-        self.modifyByIDs(type, ids: [realmObject.id], modify: modify)
-    }
-}
-
-// MARK: データ削除
-public extension NBRealmAccessor {
-    
-    /// 指定した条件のRealmオブジェクトを一括で削除する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter predicate: 条件
-    public func removeWithPredicate<T : NBRealmObject>(type: T.Type, predicate: NSPredicate) {
-        let r = self.realm
-        try! r.write {
-            r.delete(r.objects(type).filter(predicate))
-        }
-    }
-    
-    /// 指定したIDのRealmオブジェクトを一括で削除する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter ids: IDの配列
-    public func removeByIDs<T : NBRealmObject>(type: T.Type, ids: [Int64]) {
-        self.removeWithPredicate(type, predicate: self.predicateIDs(ids))
-    }
-    
-    /// Realmオブジェクトの配列を渡して一括で削除する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter realmObjects: 削除するRealmオブジェクトの配列
-    public func remove<T : NBRealmObject>(type: T.Type, _ realmObjects: [T]) {
-        let ids = realmObjects.map { return $0.id }
-        self.removeByIDs(type, ids: ids)
-    }
-    
-    /// RealmオブジェクトのRealmSwift.List、またはRealmSwift.Resultを渡して一括で削除する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter realmObjects: 削除するRealmオブジェクトの配列
-    public func remove<T : NBRealmObject, S: SequenceType where S.Generator.Element: NBRealmObject>(type: T.Type, _ realmObjects: S) {
-        let array = realmObjects.map { return $0 as NBRealmObject }
-        self.remove(type, array)
-    }
-    
-    /// 渡したRealmオブジェクトを削除する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter realmObject: 削除するRealmオブジェクト
-    public func remove<T : NBRealmObject>(type: T.Type, _ realmObject: T) {
-        self.removeByIDs(type, ids: [realmObject.id])
-    }
-}
-
-// MARK: データ取得
-public extension NBRealmAccessor {
-    
-    /// 指定した条件・ソートでRealmオブジェクトを配列で取得する
-    /// - parameter type: Realmオブジェクト型
+    /// 指定した条件で抽出されるレコードを更新する
     /// - parameter condition: 条件オブジェクト
-    /// - parameter sort: ソート([プロパティ名 : 昇順/降順]の辞書)
-    /// - returns: Realmオブジェクトの配列
-    public func fetch<T : NBRealmObject>(type: T.Type, condition: NSPredicate? = nil, sort: [String : Bool]? = [NBRealmObjectIDKey: false]) -> [T] {
-        return self.getResults(type, condition: condition, sort: sort).map { return $0 }
+    /// - parameter updating: データ更新クロージャ
+    public func update(ids: [Int64], updating: NBRealmUpdateClosure) {
+        
     }
     
-    /// 指定した条件のRealmオブジェクト数を取得する
-    /// - parameter type: Realmオブジェクト型
+    /// 指定した条件で抽出されるレコードを更新する
     /// - parameter condition: 条件オブジェクト
-    /// - returns: Realmオブジェクトの配列
-    public func count<T : NBRealmObject>(type: T.Type, condition: NSPredicate? = nil) -> Int {
-        return self.getResults(type, condition: condition).count
-    }
-    
-    /// 指定した条件・ソートでの結果オブジェクトを取得する
-    /// - parameter type: Realmオブジェクト型
-    /// - parameter condition: 条件オブジェクト
-    /// - parameter sort: ソート([プロパティ名 : 昇順/降順]の辞書)
-    /// - returns: Realmオブジェクトの配列
-    private func getResults<T : NBRealmObject>(type: T.Type, condition: NSPredicate? = nil, sort: [String : Bool]? = nil) -> RealmSwift.Results<T> {
-        var result = self.realm.objects(type)
-        if let f = condition {
-            result = result.filter(f)
-        }
-        if let s = sort {
-            s.forEach {
-                result = result.sorted($0.0, ascending: $0.1)
-            }
-        }
-        return result
+    /// - parameter updating: データ更新クロージャ
+    public func update(entity: Entity, updating: NBRealmUpdateClosure) {
+        
     }
 }
-
-// MARK: 検索条件
-public extension NBRealmAccessor {
-    
-    /// 複数の検索条件を1つにまとめて返却する
-    /// - parameter conditions: 検索条件文字列の配列
-    /// - parameter type: 結合種別
-    /// - returns: 検索条件オブジェクト
-    public func compoundPredicateWithConditions(conditions: [String], type: NSCompoundPredicateType = .AndPredicateType) -> NSPredicate {
-        var predicates = [NSPredicate]()
-        for condition in conditions {
-            predicates.append(NSPredicate(format: condition))
-        }
-        return self.compoundPredicateWithPredicates(predicates, type: type)
-    }
-    
-    /// 複数の検索条件を1つにまとめて返却する
-    /// - parameter predicates: 検索条件オブジェクトの配列
-    /// - parameter type: 結合種別
-    /// - returns: 検索条件オブジェクト
-    public func compoundPredicateWithPredicates(predicates: [NSPredicate], type: NSCompoundPredicateType = .AndPredicateType) -> NSPredicate {
-        let ret: NSPredicate
-        switch type {
-        case .AndPredicateType: ret = NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
-        case .OrPredicateType:  ret = NSCompoundPredicate(orPredicateWithSubpredicates:  predicates)
-        case .NotPredicateType: ret = NSCompoundPredicate(notPredicateWithSubpredicate:  self.compoundPredicateWithPredicates(predicates))
-        }
-        return ret
-    }
-    
-    /// IDの配列からのIN条件の検索条件を生成する
-    /// - parameter ids: IDの配列
-    /// - returns: 検索条件オブジェクト
-    public func predicateIDs(ids: [Int64]) -> NSPredicate {
-        return NSPredicate(format: "\(NBRealmObjectIDKey) IN {%@}", argumentArray: ids.map { return NSNumber(longLong: $0) } )
-    }
-    
-    /// あいまい検索を行うための検索条件を生成する
-    /// - parameter property: プロパティ名
-    /// - parameter q: 検索文字列
-    /// - returns: 検索条件オブジェクト
-    public func predicateContainsString(property: String, _ q: String) -> NSPredicate {
-        return NSPredicate(format: "\(property) CONTAINS '\(q)'")
-    }
-    
-    /// 値の完全一致検索を行うための検索条件を生成する
-    /// - parameter property: プロパティ名
-    /// - parameter v: 値
-    /// - returns: 検索条件オブジェクト
-    public func predicateEqauls(property: String, _ v: AnyObject) -> NSPredicate {
-        return NSPredicate(format: "\(property) = %@", argumentArray: [v])
-    }
-}
-
-// MARK: ソート
-public extension NBRealmAccessor {
-    
-    /// IDでソートするための辞書を返す
-    /// - parameter ascending: 昇順/降順
-    /// - parameter seed: 既にソート用の辞書がある場合に渡すと、追記される
-    /// - returns: ソート用の辞書
-    public func sortID(ascending: Bool = true, seed: [String : Bool] = [:]) -> [String : Bool] {
-        return self.sortOf(NBRealmObjectIDKey, ascending: ascending, seed: seed)
-    }
-    
-    /// 作成日時でソートするための辞書を返す
-    /// - parameter ascending: 昇順/降順
-    /// - parameter seed: 既にソート用の辞書がある場合に渡すと、追記される
-    /// - returns: ソート用の辞書
-    public func sortCreated(ascending: Bool = true, seed: [String : Bool] = [:]) -> [String : Bool] {
-        return self.sortOf(NBRealmObjectCreatedKey, ascending: ascending, seed: seed)
-    }
-    
-    /// 作成日時でソートするための辞書を返す
-    /// - parameter ascending: 昇順/降順
-    /// - parameter seed: 既にソート用の辞書がある場合に渡すと、追記される
-    /// - returns: ソート用の辞書
-    public func sortModified(ascending: Bool = true, seed: [String : Bool] = [:]) -> [String : Bool] {
-        return self.sortOf(NBRealmObjectModifiedKey, ascending: ascending, seed: seed)
-    }
-    
-    /// 指定したプロパティでソートするための辞書を返す
-    /// - parameter property: プロパティ名
-    /// - parameter ascending: 昇順/降順
-    /// - parameter seed: 既にソート用の辞書がある場合に渡すと、追記される
-    /// - returns: ソート用の辞書
-    public func sortOf(property: String, ascending: Bool = true, seed: [String : Bool] = [:]) -> [String : Bool] {
-        var seed = seed
-        seed[property] = ascending
-        return seed
-    }
-}
-
-// MARK: - RealmSwift拡張 -
-extension RealmSwift.List {
-    
-    /// 自身のデータを配列に変換する
-    /// - returns: 変換された配列
-    func toArray() -> [Element] {
-        return self.map { return $0 }
-    }
-    
-    /// 自身のデータを渡された配列でリセットする
-    /// - parameter array: 配列
-    func resetWithArray(array: [Element]) {
-        self.removeAll()
-        self.appendContentsOf(array)
-    }
-}
-
-// MARK: - RealmSwift拡張 -
-public extension String {
-    
-    /// Realm用にエスケープした文字列
-    public var stringEscapedForRealm: String {
-        return self.replace("\\", "\\\\").replace("'", "\\'")
-    }
-}
-*/
