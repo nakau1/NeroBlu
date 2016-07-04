43965d7c574c24bd5cb1d5870276c2299ba79358
中安佑一 <yuichi.nakayasu@gmail.com>
2016-07-05 04:09:18 +0900

    Realmリストの拡張を実装

--- NeroBlu/NBRealm.swift
+++ NeroBlu/NBRealm.swift
@@ -57,3 +57,22 @@ public extension NBRealm {
 		return max.id + 1
 	}
 }
+
+// MARK: - RealmSwift.List拡張 -
+public extension RealmSwift.List {
+    
+    public typealias Entity = Element
+    
+    /// 自身のデータを配列に変換する
+    /// - returns: 変換された配列
+    public var list: [Entity] {
+        return self.map { return $0 }
+    }
+    
+    /// 自身のデータを渡された配列でリセットする
+    /// - parameter array: 配列
+    public func reset(array: [Entity] = []) {
+        self.removeAll()
+        self.appendContentsOf(array)
+    }
+}
