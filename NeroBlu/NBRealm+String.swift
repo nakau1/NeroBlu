1810117203b0984d1ae692ffdf00b85cf297f856
中安佑一 <yuichi.nakayasu@gmail.com>
2016-07-05 04:08:18 +0900

    Realm用文字列拡張を実装

--- NeroBlu/NBRealm+String.swift
+++ NeroBlu/NBRealm+String.swift
@@ -4,15 +4,19 @@
 // =============================================================================
 import UIKit
 
-// MARK: - RealmSwift拡張 -
+// MARK: - String拡張 -
 public extension String {
 	
 	/// Realm用にエスケープした文字列
-	public var stringEscapedForRealm: String {
-		
-		
-		
-		return ""
-		//return self.replace("\\", "\\\\").replace("'", "\\'")
+	public var realmEscaped: String {
+        let reps = [
+            "\\" : "\\\\",
+            "'"  : "\\'",
+            ]
+        var ret = self
+        for rep in reps {
+            ret = self.stringByReplacingOccurrencesOfString(rep.0, withString: rep.1)
+        }
+        return ret
 	}
 }
