bbfe1e448d50af547dfbed3dd1163d9b2c21a3b0
中安佑一 <yuichi.nakayasu@gmail.com>
2016-07-05 18:27:34 +0900

    RealmSwift.List拡張を別ファイルに移動した

---
+++ NeroBlu/NBRealm+RealmSwift.List.swift
@@ -0,0 +1,25 @@
+// =============================================================================
+// NBRealm
+// Copyright (C) NeroBlu. All rights reserved.
+// =============================================================================
+import UIKit
+import RealmSwift
+
+// MARK: - RealmSwift.List拡張 -
+public extension RealmSwift.List {
+    
+    public typealias Entity = Element
+    
+    /// 自身のデータを配列に変換する
+    /// - returns: 変換された配列
+    public var list: [Entity] {
+        return self.map { return $0 }
+    }
+    
+    /// 自身のデータを渡された配列でリセットする
+    /// - parameter array: 配列
+    public func reset(array: [Entity] = []) {
+        self.removeAll()
+        self.appendContentsOf(array)
+    }
+}
