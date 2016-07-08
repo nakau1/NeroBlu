5fad1b7dde93a25fecc02bdb3c3cf2be474eece0
中安佑一 <yuichi.nakayasu@gmail.com>
2016-07-09 04:13:44 +0900

    ビューコントローラを初期化

--- NeroBluDemo/ViewController.swift
+++ NeroBluDemo/ViewController.swift
@@ -4,17 +4,10 @@
 //
 import UIKit
 import NeroBlu
-import RealmSwift
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let p = NSPredicate("age", greaterThan: 12).any(of: "student")
-            .and(NSPredicate(isNotNil: "email"))
-        .and(NSPredicate("created", fromDate: nil, toDate: NSDate()))
-        
-        print(p)
     }
 }
