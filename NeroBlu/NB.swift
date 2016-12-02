bc850e97080699e083461b18559505e1f928d025
中安佑一 <yuichi.nakayasu@gmail.com>
2016-12-02 14:06:59 +0900

    オプショナルの値も渡せるprint関数を定義

--- NeroBlu/NB.swift
+++ NeroBlu/NB.swift
@@ -17,3 +17,14 @@ public typealias CompletionHandlerWithError = (NSError?) -> ()
 
 /// 処理完了時のハンドラクロージャ(結果ステータス付き)
 public typealias CompletionHandlerWithResult = (NBResult) -> ()
+
+/// オプショナルの値も渡せるprint関数
+/// - parameter items: 値(可変長引数)
+/// - parameter separator: セパレータ文字
+/// - parameter terminator: 終了文字
+public func print(_ items: Any?..., separator: String = ", ", terminator: String = "\n") {
+    for (i, item) in items.enumerated() {
+        let t = (i == items.lastIndex!) ? terminator : separator
+        print(item ?? NSNull(), terminator: t)
+    }
+}
